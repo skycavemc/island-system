a0a8b7ede43122359f0e40c9335d258b8fc0920f
Leon Heuer <leon.heuer@outlook.de>
2022-02-08 19:14:01 +0100

    Fixed limit manager

--- src/main/java/de/leonheuer/skycave/islandsystem/manager/LimitManager.java
+++ src/main/java/de/leonheuer/skycave/islandsystem/manager/LimitManager.java
@@ -1,93 +1,87 @@
 package de.leonheuer.skycave.islandsystem.manager;
 
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
-import com.sk89q.worldguard.WorldGuard;
-import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import de.leonheuer.skycave.islandsystem.IslandSystem;
 import de.leonheuer.skycave.islandsystem.enums.EntityLimit;
-import de.leonheuer.skycave.islandsystem.models.IslandCount;
-import org.bukkit.Bukkit;
 import org.bukkit.World;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class LimitManager {
 
     private final IslandSystem main;
-    private final HashMap<ProtectedRegion, IslandCount> limitsMap = new HashMap<>();
+    private Map<String, Map<EntityType, Integer>> entityCountMap = new HashMap<>();
 
     public LimitManager(IslandSystem main) {
         this.main = main;
     }
 
-    public void start(World world) {
-        RegionManager rm = WorldGuard.getInstance().getPlatform().getRegionContainer().get(BukkitAdapter.adapt(world));
+    public void start(@NotNull World world) {
+        RegionManager rm = main.getRegionContainer().get(BukkitAdapter.adapt(world));
         if (rm == null) {
             return;
         }
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                limitsMap.clear();
-                HashMap<ProtectedRegion, IslandCount> count = new HashMap<>();
-                for (Entity entity : Bukkit.getWorld("skybeeisland").getEntities()) {
-                    if (!(entity instanceof LivingEntity)) {
+
+        main.getLogger().info("Started limit manager for world " + world.getName());
+        main.getServer().getScheduler().runTaskTimer(main, () -> {
+            Map<String, Map<EntityType, Integer>> newMap = new HashMap<>();
+            for (LivingEntity e : world.getEntitiesByClass(LivingEntity.class)) {
+                if (EntityLimit.getLimitByType(e.getType()) == -1) {
+                    continue;
+                }
+
+                BlockVector3 loc = BukkitAdapter.asBlockVector(e.getLocation());
+                Set<ProtectedRegion> regions = rm.getApplicableRegions(loc).getRegions();
+                if (regions.isEmpty()) {
+                    continue;
+                }
+
+                for (ProtectedRegion r : regions) {
+                    if (!r.getId().matches("^[s][c][_]\\d{3}$")) {
                         continue;
                     }
-                    if (EntityLimit.getLimitByType(entity.getType()) == -1) {
-                        continue;
-                    }
-                    ApplicableRegionSet set = rm.getApplicableRegions(BukkitAdapter.asBlockVector(entity.getLocation()));
-                    if (set.getRegions().size() == 0) {
-                        continue;
-                    }
-                    for (ProtectedRegion region : set.getRegions()) {
-                        if (!region.getId().matches("^[s][c][_]\\d{3}$")) {
-                            continue;
-                        }
-                        if (!count.containsKey(region)) {
-                            count.put(region, new IslandCount());
-                        }
-                        count.get(region).incEntityCount(entity.getType());
-                        setEntityCount(region, entity.getType(), count.get(region).getEntityCount(entity.getType()));
-                    }
+                    addToMap(newMap, r.getId(), e.getType());
                 }
             }
-        }.runTaskTimer(main, 0L, 200L);
+            entityCountMap = newMap;
+        }, 0L, 40L);
     }
 
-    public void incEntityCount(ProtectedRegion region, EntityType type) {
-        if (!limitsMap.containsKey(region)) {
-            limitsMap.put(region, new IslandCount());
+    public void addToMap(Map<String, Map<EntityType, Integer>> map, String id, EntityType type) {
+        if (!map.containsKey(id)) {
+            Map<EntityType, Integer> entities = new HashMap<>();
+            entities.put(type, 1);
+            map.put(id, entities);
+            return;
         }
-        limitsMap.get(region).incEntityCount(type);
+
+        Map<EntityType, Integer> entities = map.get(id);
+        if (!entities.containsKey(type)) {
+            entities.put(type, 1);
+            return;
+        }
+
+        entities.put(type, entities.get(type) + 1);
     }
 
-    public void decEntityCount(ProtectedRegion region, EntityType type) {
-        if (!limitsMap.containsKey(region)) {
-            limitsMap.put(region, new IslandCount());
+    public int getEntityCount(String regionId, EntityType type) {
+        if (!entityCountMap.containsKey(regionId)) {
+            return 0;
         }
-        limitsMap.get(region).decEntityCount(type);
+        return entityCountMap.get(regionId).getOrDefault(type, 0);
     }
 
-    public int getEntityCount(ProtectedRegion region, EntityType type) {
-        if (!limitsMap.containsKey(region)) {
-            limitsMap.put(region, new IslandCount());
-        }
-        return limitsMap.get(region).getEntityCount(type);
-    }
-
-    public void setEntityCount(ProtectedRegion region, EntityType type, int count) {
-        if (!limitsMap.containsKey(region)) {
-            limitsMap.put(region, new IslandCount());
-        }
-        limitsMap.get(region).setEntityCount(type, count);
+    public Map<String, Map<EntityType, Integer>> getEntityCountMap() {
+        return entityCountMap;
     }
 
 }
